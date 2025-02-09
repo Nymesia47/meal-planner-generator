dce07aad5c6bfc2f01644c3ceef47c83f6ebba15
Gabriella Calvano <gabcalvano@gmail.com>
2025-02-09 23:46:31 +0100

    added meal type selection functionality to the new meal form

--- src/components/parts/AddMealForm.jsx
+++ src/components/parts/AddMealForm.jsx
@@ -4,35 +4,76 @@ import PropTypes from "prop-types";
 function AddMealForm({handleSubmit, addMeal}) {
   const [mealTitle, setMealTitle] = useState("");
   const [errorMsg, setErrorMsg] = useState("");
+  const mealTypeOptions = ["Breakfast", "Lunch", "Dinner", "Side", "Dessert"];
+  const [mealType, setMealType] = useState([]);
 
-  const handleAddMeal = ()=> {
+  const handleAddMeal = (ev)=> {
+    ev.preventDefault();
 
-    if (!mealTitle) {
-      setErrorMsg(<p>A meal title is required</p>);
+    if (!mealTitle || mealType.length === 0) {
+      setErrorMsg(
+        <p className="errorMsg">
+          {!mealTitle ? "A meal title is required. " : ""}
+          {mealType.length === 0 ? "At least one type must be selected." : ""}
+        </p>
+      );
+      return;
     }
+
 
     const newMeal = {
       title: mealTitle,
-      plate_id: Date.now()
-    }
+      plate_id: Date.now(),
+      type: mealType
+    };
 
-    {addMeal(newMeal)};
+    addMeal(newMeal);
   }
+
+  const handleChangeType = (ev)=> {
+    setMealType((prev) => ev.target.checked ? [...prev, ev.target.value] : prev.filter((t) => t !== ev.target.value));
+  }
+
+  //Renders type of meal checkbox
+  const typeSelection = mealTypeOptions.map((type) => (
+    <li key={type}>
+      <label >
+        <input
+          type="checkbox"
+          value={type}
+          name="type[]"
+          onChange={handleChangeType}
+          
+        />
+        {" "}{type}
+      </label>
+    </li> 
+  ))
 
   return (
     <section>
       <h2 className="form-title" >Add a meal to your library</h2>
       <form className="form" onSubmit={handleSubmit}>
-        <label htmlFor="title">Meal:</label>
-        <input
-          type="text"
-          id="title"
-          name="title"
-          value={mealTitle}
-          className="form-text-input"
-          onChange={(ev) => setMealTitle(ev.target.value)}
-          required
-        />
+        <label htmlFor="title">
+          Meal:
+          <input
+            type="text"
+            id="title"
+            name="title"
+            value={mealTitle}
+            className="form-text-input"
+            onChange={(ev) => setMealTitle(ev.target.value)}
+            required
+          /> 
+        </label>
+        <fieldset>
+          <legend id="type-selection-legend">Select type of meal:</legend>
+
+          <ul role="group" aria-labelledby="type-selection-legend">
+            {typeSelection}
+          </ul>
+        </fieldset>
+        
         {errorMsg}
       <button className="form-button" type="submit" onClick={handleAddMeal}> Add Meal</button>
       </form>
